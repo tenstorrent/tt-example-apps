4d501cbabd27fa14fccb1fdf7bc9780be15489f1
Connor Guerrero <cguerrero@tenstorrent.com>
2025-05-16 17:28:37 -0700

    Add PDF RAG example

---
+++ example-apps/pdf_rag/README.md
@@ -0,0 +1,39 @@
+# 📄 Chat with a PDF using RAG
+This is a **Streamlit** app that allows you to chat with a PDF file using an LLM and Retrieval Augmented Generation (RAG).  
+
+This example requires an active Tenstorrent instance running on Koyeb.  To deploy your first service using Tenstorrent instances on Koyeb, refer to Koyeb's [tenstorrent-examples repository](https://github.com/koyeb/tenstorrent-examples).
+
+## Demo
+
+https://github.com/user-attachments/assets/3db8e3af-3d4b-4d4a-b0ec-32d86f2b5e6d
+
+## Features
+- Upload a PDF file
+- Ask questions about the content of the file
+- Receive accurate answers using RAG
+
+## Getting started
+
+### 1. Clone the GitHub repository
+```bash
+git clone https://github.com/tenstorrent/tt-example-apps.git
+cd tt-example-apps/example-apps/pdf_rag
+```
+
+### 2. Install the required dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Run the Streamlit app
+```bash
+streamlit run pdf_rag.py
+```
+
+## How it works
+
+- PDF file contents are loaded using **PyPDF2** and split into chunks.
+- Embeddings are created and stored using **Chroma**.
+- Relevant context is retrieved based on the user's query, then added to the LLM input message.
+- A request containing the input message and context is sent to the Tenstorrent instance, which runs the LLM inference.
+- The app receives the response and displays the generated text.
---
+++ example-apps/pdf_rag/pdf_rag.py
@@ -0,0 +1,131 @@
+from urllib.parse import urljoin
+
+import requests
+import streamlit as st
+import PyPDF2
+import chromadb
+
+
+@st.cache_data
+def get_available_models(base_url):
+    res = requests.get(urljoin(base_url, "/v1/models"), headers={"accept": "application/json"})
+    return res
+
+
+@st.cache_resource
+def setup_chromadb():
+    chroma = chromadb.Client()
+    col = chroma.get_or_create_collection("pdf_rag")
+    return col
+
+
+def process_pdf(pdf, col):
+    chunks = chunk(extract_text(pdf))
+
+    # Clear DB if uploading new PDF file
+    col_ids = col.get()["ids"]
+    if col_ids:
+        col.delete(ids=col_ids)
+
+    col.add(documents=chunks, ids=[str(i) for i in range(len(chunks))])
+
+
+def extract_text(pdf):
+    return "".join(p.extract_text() for p in PyPDF2.PdfReader(pdf).pages)
+
+
+def chunk(text, size=100, overlap=30):
+    words = text.split()
+    return [" ".join(words[i:i+size]) for i in range(0, len(words), size - overlap)]
+
+
+def call_chat_completion(query, context, model_name, tt_base_url):
+    system_message = {
+        "role": "system",
+        "content":
+            f"""
+            You are a helpful assistant. \n
+            When answering user questions, use the provided information below to answer the user question accurately.\n
+            Base your responses on the retrieved context, especially for summary or factual queries.\n
+            Provide clear and concise answers.
+
+            {context}
+            """
+    }
+
+    user_message = {"role": "user", "content": query}
+
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "model": model_name,
+        "messages": [system_message, user_message],
+        "max_tokens": 200
+    }
+
+    CHAT_ENDPOINT = "/v1/chat/completions"
+    res = requests.post(urljoin(tt_base_url, CHAT_ENDPOINT), headers=headers, json=payload)
+
+    if res.status_code != 200:
+        return f"Error: {res.status_code} - {res.text}"
+    return res.json()["choices"][0]["message"]["content"].strip()
+
+
+def main():
+    st.title("📄 PDF RAG")
+    st.caption("Chat with a PDF using LLM + RAG")
+
+    col = setup_chromadb()
+
+    tt_base_url = st.text_input(
+        "Enter the public URL of your Tenstorrent instance on Koyeb.")
+
+    if tt_base_url:
+        # Fetch available models from Tenstorrent instance
+        model_name_response = get_available_models(tt_base_url)
+
+        if model_name_response.status_code != 200:
+            st.write("Error fetching model names from instance.")
+            model_name = st.text_input("Enter the name of the model")
+        else:
+            available_models = [m['id'] for m in model_name_response.json()['data']]
+
+            model_name = st.selectbox(
+                "Select the LLM to use.",
+                available_models,
+                help=f"These are the available models on {tt_base_url}"
+            )
+
+        # PDF upload
+        pdf = st.file_uploader("Upload a PDF", type="pdf")
+
+        if "pdf_processed" not in st.session_state:
+            st.session_state['pdf_processed'] = False
+            st.session_state['last_filename'] = None
+
+        if pdf:
+            if pdf.name != st.session_state['last_filename']:
+                st.session_state['pdf_processed'] = False
+                st.session_state['last_filename'] = pdf.name
+
+            if not st.session_state['pdf_processed']:
+                with st.spinner("🗂️ Processing PDF..."):
+                    process_pdf(pdf, col)
+                    st.session_state['pdf_processed'] = True
+                    st.session_state['query_input'] = ""
+                st.success("PDF indexed!")
+
+            # User question
+            query = st.text_input("Ask a question about the PDF", key='query_input')
+            if query:
+                with st.spinner("💬 Generating answer..."):
+                    docs = col.query(query_texts=[query], n_results=3)['documents'][0]
+                    context = "\n".join(docs)
+
+                    response = call_chat_completion(query, context, model_name, tt_base_url)
+
+                    st.markdown("**Answer:**")
+                    st.write(response)
+
+
+if __name__ == "__main__":
+    main()
